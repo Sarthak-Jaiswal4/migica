f1a630ae94a1ed36080e435bd17d0d78e33ba97b
Sarthak Jaiswal <sarthakjazz8@gmail.com>
2026-04-11 04:49:36 +0530

    file changed

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,11 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-    console.log("Missing Supabase URL or Anon Key", process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-    throw new Error("Missing Supabase URL or Anon Key")
-}
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
-export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+// if (!supabaseUrl || !supabaseAnonKey) {
+//     console.warn("Supabase credentials are missing. Authentication and database features will not work correctly.")
+// }
+
+// export const supabase = createClient(
+//     supabaseUrl,
+//     supabaseAnonKey
+// )
